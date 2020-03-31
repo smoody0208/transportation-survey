b9aab5018d0a5552caebc68b6dffd9392971db22
Spencer Moody <spencer.moody@outlook.com>
2020-03-31 13:41:59 -0700

    Add content to html and functionality to scripts

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $("#work-responses").append(workTransportationMode +"<br>");
+    })
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $("#fun-responses").append(funTransporationMode + "<br>");
+    })
+  })
+})
